fbea529ba420549b273360fce88e4e769b8c3856
Andrew Butenko <andrew.butenko@novasystems.com>
2021-07-19 21:21:35 +0930

    initial setup

---
+++ constants.py
@@ -0,0 +1,9 @@
+TELEGRAM_TOKEN = '***'
+STRIPE_PAYMENT_TOKEN = '***'
+HEROKU_PORT = '***'
+HEROKU_APP_NAME = '***'
+MONGODB_CONNECTION_URL = '***'
+IBM_SERVICE_URL = '***'
+IBM_API_URL = '***'
+IBM_KEY = '***'
+
